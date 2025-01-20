7b6845e98b0c3523dc06b22fff3f635dbb2416b0
sartajahamad <sartaj1aha2@gmail.com>
2025-01-21 00:41:07 +0530

    Updated code with new features or fixes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
+import langchain
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
@@ -12,8 +13,6 @@ import os
 import pytesseract
 from PIL import Image
 from fpdf import FPDF
-import langchain
-from langchain.vectorstores import FAISS
 
 
 # Configure Tesseract OCR
